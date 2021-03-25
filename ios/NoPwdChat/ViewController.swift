e47ca653fbd08d5b0a161f5bb76a1b25a3c98223
cardoso <matheus@cardo.so>
2021-03-25 18:55:20 -0300

    Small changes

--- ios/NoPwdChat/ViewController.swift
+++ ios/NoPwdChat/ViewController.swift
@@ -13,10 +13,10 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        Lock.passwordless(clientId: "l2ayolIXyJLlumJI5iKyhqkmJP6CObue", domain: "dev-6dpur00z.us.auth0.com")
+        Lock.passwordless(clientId: "1HBJhBfsj1cbypHlSU85L4Ve2YD2RrMh", domain: "dev-fhjteaie.us.auth0.com")
             .withOptions { $0.oidcConformant = true }
             .withConnections { $0.email(name: "email") }
-            .withStyle { $0.title = "Welcome to my App!" }
+            .withStyle { $0.title = "Passwordless Chat" }
             .onAuth(callback: onAuth)
             .onError { print("Failed with \($0)") }
             .onCancel { print("User cancelled") }
